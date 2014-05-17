2af88a2a9db7f8bea09c657d767cb9630cd93ec9
Christian Iwanzik <christian.iwanzik@gmail.com>
2014-05-17 16:21:09 +0200

    solving Section 1

--- src/main/scala/section1/Section1.scala
+++ src/main/scala/section1/Section1.scala
@@ -6,15 +6,18 @@ object Section1 {
 
   // implement the ???
   // and change if necessary the 'def' with 'val', 'var' or 'lazy val'
-  def message: String = ???
-  def greeting: String = ???
-  def max(x: Int, y: Int): Int = ???
+  def message: String = "Hello, World"
+  var greeting: String = "Hello, World"
+
+  def max(x: Int, y: Int): Int = if (x > y) x else y
+
+  def newConstOne(x: Int, y: => Int) = 1
 
   def expr(): String = {
     var result = ""
     // change if necessary the 'def' with 'val', 'var' or 'lazy val'
-    def x = { result += "x"; 1 }
-    def y = { result += "y"; 2 }
+    val x = { result += "x"; 1 }
+    lazy val y = { result += "y"; 2 }
     def z = { result += "z"; 3 }
     z + y + x + z + y + x
     result
--- src/test/scala/section1/Section1Test.scala
+++ src/test/scala/section1/Section1Test.scala
@@ -10,7 +10,7 @@ class Section1Test extends FunSuite {
 
   test("Section1 wants to be alone") {
     assert(Section1.greeting === "Hello, World")
-    //Section1.greeting = "Leave me alone, world!"
+    Section1.greeting = "Leave me alone, world!"
     assert(Section1.greeting === "Leave me alone, world!")
   }
 
