843db8d669d2e83da0ca0ade18db1024515adb55
Christian Iwanzik <c.iwanzik@tarent.de>
2014-05-21 07:52:21 +0200

    Fixing test description

--- src/test/scala/section1/Section1Test.scala
+++ src/test/scala/section1/Section1Test.scala
@@ -23,7 +23,7 @@ class Section1Test extends FunSuite {
     assert(Section1.expr() === "xzyz")
   }
 
-  test("'mangleSomeInts' function using call by name can be called with function 'exception'") {
+  test("'newConst' function using call by name can be called with function 'exception'") {
     assert(Section1.newConst(2, Section1.exception) === 2)
     assert(Section1.newConst(4, Section1.exception) === 4)
   }
