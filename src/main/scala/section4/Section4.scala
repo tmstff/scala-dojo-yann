69d3aaf5a88628ba80e7ef09659a4557bd4fda0c
Christian <dark-toast@gmx.de>
2014-08-28 13:17:14 +0200

    Adding tests for traits

--- src/main/scala/section4/Section4.scala
+++ src/main/scala/section4/Section4.scala
@@ -1,21 +1,30 @@
 package section4
 
+import com.fasterxml.jackson.annotation.{PropertyAccessor, JsonIgnore}
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility
+
 object Section4 {
 
-  class Cigarettes
-  case class UnderAgeException(message: String) extends Exception(message)
-  case class NoIdException(message: String) extends Exception(message)
+  trait JsonConvertible {
+    @JsonIgnore
+    val mapper = new ObjectMapper
+    mapper.registerModule(DefaultScalaModule)
+    mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY)
 
-  case class Customer(age: Int, haveId: Boolean = false) {
-    // hint: you can use the require function
+    @JsonIgnore
+    def toJson: String = mapper.writeValueAsString(this)
   }
 
-  class Store {
-    def buyCigarettes(customer: Customer): Cigarettes = ???
-  }
+  case class Book(title: String, pages: Int)
+  case class House(rooms: Int, floors: Int, street: String)
 
-  class LaxistStore extends Store {
-    override def buyCigarettes(customer: Customer): Cigarettes = ???
-  }
+  // Returns a new Book object
+  def getABook(title: String, pages: Int): Book = ???
+
+  // Returns a new House object
+  def getAHouse(rooms: Int, floors: Int, street: String): House = ???
+
 
 }
--- src/test/scala/section4/Section4Test.scala
+++ src/test/scala/section4/Section4Test.scala
@@ -5,43 +5,28 @@ import section4.Section4._
 
 class Section4Test extends FunSuite with Matchers {
 
-  test("a customer under 18 is not allowed to buy cigarettes") {
-    intercept[UnderAgeException] {
-      new Store().buyCigarettes(Customer(15))
-    }
+  test("a custom book should be convertible in a json notation") {
+    val book: Book = getABook("Programming in Scala", 852)
+
+    book shouldBe a [Book]
+    book shouldBe a [JsonConvertible]
+
+    // Only to make the tests compilable without implementation.
+    // We also can call house.toJson
+    val jsonable: JsonConvertible = book.asInstanceOf[JsonConvertible]
+    jsonable.toJson shouldBe """{"title":"Programming in Scala","pages":852}"""
   }
 
-  test("a customer not allowed to buy cigarettes should be informed why") {
-    val thrown = the [UnderAgeException] thrownBy {
-      new Store().buyCigarettes(Customer(15))
-    }
-    thrown.getMessage shouldEqual "Customer must be older than 18 but was 15"
-  }
+  test("a custom house should be convertible in a json notation") {
+    val house: House = getAHouse(4, 2, "Fakestreet")
 
-  test("a customer with 18 is allowed to buy cigarettes if he can show an ID") {
-    new Store().buyCigarettes(Customer(18, true)) shouldBe a [Cigarettes]
-  }
+    house shouldBe a [House]
+    house shouldBe a [JsonConvertible]
 
-  test("a customer with 18 is not allowed to buy cigarettes if he cannot show an ID") {
-    intercept[NoIdException] {
-      new Store().buyCigarettes(Customer(18, false))
-    }
-  }
-
-  test("a customer's age must be positive") {
-    intercept[IllegalArgumentException] {
-      Customer(-2)
-    }
-  }
-
-  test("a customer under 18 is not allowed to buy cigarettes, even in a laxist store") {
-    intercept[UnderAgeException] {
-      new LaxistStore().buyCigarettes(Customer(15))
-    }
-  }
-
-  test("a customer over 18 is allowed to buy cigarettes, even without ID in a laxist store") {
-    new LaxistStore().buyCigarettes(Customer(18, false)) shouldBe a [Cigarettes]
+    // Only to make the tests compilable without implementation.
+    // We also can call house.toJson
+    val jsonable: JsonConvertible = house.asInstanceOf[JsonConvertible]
+    jsonable.toJson shouldBe """{"rooms":4,"floors":2,"street":"Fakestreet"}"""
   }
 
 
