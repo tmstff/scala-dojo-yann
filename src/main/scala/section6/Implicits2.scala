55350d0839e250f66255e83c27b5d9fd1abcb496
Christian <dark-toast@gmx.de>
2014-10-30 15:14:18 +0100

    New cheatsheets for implicits

---
+++ src/main/scala/section6/Implicits2.scala
@@ -0,0 +1,66 @@
+object Implicits2 extends App {
+
+  /**
+   * In this example we define a new class Rational which represents
+   * rational number with a numerator and a denominator.
+   *
+   * With a self defined "+"-method, we can add another Rational
+   * and create a new Rational with the added values.
+   */
+  case class Rational(numer: Int, denom: Int) {
+    def + (r: Rational): Rational = Rational(
+      r.numer * denom + numer * r.denom,
+      r.denom * denom
+    )
+  }
+
+  // So let test this:
+  val add = Rational(1, 3) + Rational(2, 3)
+  assert(add == Rational(9, 9))
+  println(add)
+
+
+  // And now let us add a pure integer to our Rational:
+  // We will expect an result of 4/3
+  val intAdd = Rational(1, 3) + 1
+  assert(intAdd == Rational(4, 3))
+  println(intAdd)
+
+  // But how does the "+"-method know how to deal with the integer!?
+  // The method is only defined for the Rational type:
+  //  ---> def + (r: Rational)
+
+  // The answer is this companion object, which defines a implicit convert method
+  // This method takes an integer value i and converts it to a Rational(i,1)
+  // This new rational therefore, is passed as the "r"-parameter of the "+"-method.
+  //
+  // PS: The method can also be defined everywhere in the used scope.
+  object Rational {
+    implicit def fromIntToRational(value: Int): Rational = Rational(value, 1)
+  }
+
+  // Does this also works with an java Integer?
+  /*
+      val i: java.lang.Integer = 42
+      new Rational(1, 1) + i
+   */
+  // In the last example we saw, that we can convert the Integer to an int with Predef
+  // So this code should compiled to:
+  // new Rational(1, 1) + fromIntToRational(Integer2int(i)) or?
+  // No! Because the scala compiler does not nest implicits conversion cause of complexity and performance
+  // Think of the problem of possible infinite resolving and conversion loop.
+
+  // But if we are a little bit more verbose we can write:
+  val i: java.lang.Integer = 42
+  val j: Int = i
+  new Rational(1, 1) + j
+
+  // Further information:
+  // http://www.artima.com/pins1ed/implicit-conversions-and-parameters.html
+
+  // Hint!
+  // Do not add implicits as much as you can. Too much and not intuitional conversions make your code
+  // unreadable!
+}
+
+
---
+++ src/main/scala/section6/Implicits3.scala
@@ -0,0 +1,53 @@
+package section6
+
+/**
+ * Implicit parameters
+ * Implicit parameters can be used for periodic use of a single value.
+ * Such as session Ids, runtime contexts and so on.
+ */
+
+
+// First of all we define an example class.
+// Our class defines three methods:
+// * step1 is public.
+// * step1 calls step2
+// * step2 calls step3
+
+// The methods have two parameter lists. Implicit parameters must only exist in the last
+// parameter list.
+// The x defines a string.
+// The y parameter defines an int and is implicit.
+class SomeFooClass {
+
+  // The step1 method has the implicit y value.
+  // If we look at the step2 method, this has also an implicit y parameter.
+  def step1(x: String)(implicit y: Int) {
+    // So we can call step2 only with the explicit x parameter.
+    // y is add implicit.
+    step2(x)
+  }
+
+  // As step1, step2 passes the implicit value y to the step3 method.
+  private def step2(x: String)(implicit y: Int) {
+    step3(x)
+  }
+
+  // Step3 uses the implicit value y in the println function.
+  private def step3(x: String)(implicit y: Int) {
+    println(s"X is $x and Y is: $y")
+  }
+}
+
+object Implicits3 extends App {
+  // Now we will use the SomeFooClass
+  // First we apply the second parameter explicit
+  val foo = new SomeFooClass
+  foo.step1("Hallo welt")(4)
+
+  // But we can also define an implicit value in out scope.
+  // So, someInt will be automatically passed to the step1 method.
+  implicit val someInt = 24
+  foo.step1("Hallo!")
+}
+
+
