8fd52cbc6848ff852f89203dcef3e43122381327
Christian <christian.iwanzik@gmail.com>
2015-12-16 12:25:57 +0100

    Adding thunderbird solutions for the section1

--- src/main/scala/section1/Section1.scala
+++ src/main/scala/section1/Section1.scala
@@ -1,27 +1,31 @@
 package section1
 
 object Section1 {
-  def exception: Int = throw new RuntimeException
+
   def constOne(x: Int, y: Int) = 1
 
   // implement the ???
   // and change if necessary the 'def' with 'val', 'var' or 'lazy val'
-  def message: String = ???
-  def greeting: String = ???
-  def max(x: Int, y: Int): Int = ???
+  def message: String = "Hello, World"
+  var greeting: String = "Hello, World"
+  def max(x: Int, y: Int): Int = if (x > y) x else y
 
   def expr(): String = {
     var result = ""
-    // change if necessary the 'def' with 'val', 'var' or 'lazy val'
-    def x = { result += "x"; 1 }
-    def y = { result += "y"; 2 }
+    // change the 'def' with 'val', 'var' or 'lazy val'
+    // Ask yourself, when is which block executed?
+    val x = { result += "x"; 1 }
+    lazy val y = { result += "y"; 2 }
     def z = { result += "z"; 3 }
+
+    // Do not edit this line. ;-)
     z + y + x + z + y + x
     result
   }
 
-  def newConst(x: Int, y: Int) = x
+  def exception: Int = throw new RuntimeException
+  def newConst(x: Int, y: => Int) = x
 
   // Return a function which takes one parameter and multiply it with the 'x' value
-  def someIntFunction(x: Int): (Int => Int) = ???
+  def someIntFunction(x: Int): (Int => Int) = (y: Int) => x * y
 }
