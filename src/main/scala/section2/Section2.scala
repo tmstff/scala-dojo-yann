7fde84fa56040b0508d32ab381400b6f2e44cb87
Christian <dark-toast@gmx.de>
2016-01-19 09:04:43 +0100

    Solutions for section2

--- src/main/scala/section2/Section2.scala
+++ src/main/scala/section2/Section2.scala
@@ -1,27 +1,105 @@
 package section2
 
+import collection.mutable
+import scala.annotation.tailrec
+import scala.Predef._
+
 object Section2 {
 
   // Arrays
+  def loud(input: Array[String]): Array[String] = input.map(_.toUpperCase)
 
-  def loud(input: Array[String]): Array[String] = ???
+  // higher order functions
+  def concatenate(input: Array[String]): String = input.foldLeft("")(_ + " " + _).trim
 
-  def concatenate(input: Array[String]): String = ???
+  // imperative
+  def concatenate2(input: Array[String]): String = {
+    val builder = mutable.StringBuilder.newBuilder
+    for(str <- input) {
+      builder ++= str ++= " "
+    }
+    builder.toString().trim
+  }
+
+  // direct API
+  def concatenate3(input: Array[String]): String = input.mkString(" ")
 
   // Seq and List
+  def loud(input: Seq[String]): Seq[String] = input.map(_.toUpperCase)
 
-  def loud(input: Seq[String]): Seq[String] = ???
+  // Functional style
+  def extractWords(input: Seq[String]): Seq[String] = {
+    input.map(str => str.split(" ")).flatten
+    // shorter:
+    // input.flatMap(f => f.split(" "))
+  }
 
-  def extractWords(input: Seq[String]): Seq[String] = ???
+  // for comprehension
+  def extractWords2(input: Seq[String]): Seq[String] = {
+    for {
+      str <- input
+      seq <- str.split(" ")
+    } yield {
+      seq
+    }
+  }
+
+  // tail recursive style
+  def extractWords4(input: Seq[String]): Seq[String] = {
+    @tailrec
+    def step(actual: List[String], elements: Seq[String]): Seq[String] = {
+      if(elements.isEmpty) actual
+      else step(actual ::: elements.head.split(" ").toList, elements.tail)
+    }
+
+    step(Nil, input)
+  }
+
+  def extractWords5(input: Seq[String]): Seq[String] = {
+    val list = mutable.ListBuffer.newBuilder[String]
+    for(str <- input) {
+      list ++= str.split(" ").toList
+    }
+
+    list.result()
+  }
 
   // Map
+  def loud(input: Map[String, String]): Map[String, String] = input.map(t => (t._1.toUpperCase, t._2.toUpperCase))
 
-  def loud(input: Map[String, String]): Map[String, String] = ???
+  // Functional style
+  def counts2(input: Seq[String]): Map[String, Int] = input
+    .flatMap(_.split(" "))
+    .groupBy((word: String) => word)
+    .mapValues(_.length)
 
-  def counts(input: Seq[String]): Map[String, Int] = ???
+  // variable immutable map
+  def counts3(input: Seq[String]): Map[String, Int] = {
+    var result: Map[String, Int] = Map()
+    for(word <- extractWords(input)) {
+      result.get(word) match {
+        case Some(counts) => result += word -> (counts + 1)
+        case None => result += word -> 1
+      }
+    }
+    result
+  }
 
-  def addTriedCharacter(wordId: Int, character: Char): Unit = ???
+  // static mutable map
+  def counts(input: Seq[String]): Map[String, Int] = {
+    val result: mutable.Map[String, Int] = mutable.Map()
+    for(word <- extractWords(input)) {
+      result.get(word) match {
+        case Some(counts) => result += word -> (counts + 1)
+        case None => result += word -> 1
+      }
+    }
+    result.toMap
+  }
 
-  def isCharacterAlreadyTried(wordId: Int, character: Char): Boolean = ???
+  val buffer: mutable.HashSet[(Int, Char)] = new mutable.HashSet
 
+  def addTriedCharacter(wordId: Int, character: Char): Unit = buffer += ((wordId, character))
+
+  def isCharacterAlreadyTried(wordId: Int, character: Char): Boolean = buffer.contains((wordId, character))
 }
