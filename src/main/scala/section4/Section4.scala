f55f7b5646bd80859328e7283295e84fbbd0cacf
Tim Steffens <tim.steffens@rewe-digital.com>
2016-07-21 11:15:59 +0200

    added solutions for section 4

--- src/main/scala/section4/Section4.scala
+++ src/main/scala/section4/Section4.scala
@@ -13,22 +13,19 @@ object Section4 {
     val mapper = new ObjectMapper
     mapper.registerModule(DefaultScalaModule)
     mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY)
-
     @JsonIgnore
     def toJson: String = mapper.writeValueAsString(this)
   }
-
   case class Book(title: String, pages: Int)
-  case class House(rooms: Int, floors: Int, street: String)
-
+  case class House(rooms: Int, floors: Int, street: String) extends JsonConvertible
   trait BookFactory {
     def getABook(title: String, pages: Int): Book
   }
-
   class JsonConvertibleBookFactory extends BookFactory {
-    def getABook(title: String, pages: Int): Book = ???
+    def getABook(title: String, pages: Int): Book = {
+      new Book(title, pages) with JsonConvertible
+    }
   }
-
   class BookCatalogue {
     val bookFactory: BookFactory = new JsonConvertibleBookFactory
     val catalogue = Map(
@@ -40,28 +37,27 @@ object Section4 {
       "Book6" -> bookFactory.getABook("Book6", 350),
       "Book7" -> bookFactory.getABook("Book7", 400)
     )
-
     def getBook(title: String) = catalogue(title)
   }
-
   class PriceResolver {
-    def getPrice(book: Book): Int = Random.nextInt % 30
+    def getPrice(book: Book): Int = book match {
+      //Random.nextInt % 30
+      case Book("Book1", _) => 25
+      case Book("Book2", _) => 15
+      case _ => 0
+    }
   }
-
   class BookStore {
     private val bookCatalogue: BookCatalogue = new BookCatalogue
     private val priceResolver: PriceResolver = new PriceResolver
     private var earnings: Int = 0
-
     def buyABook(title: String): Book = {
       val book = bookCatalogue.getBook(title)
       earnings = earnings + priceResolver.getPrice(book)
       book
     }
-
     def actualEarnings = earnings
   }
-
 
   class Cigarettes
   case class UnderAgeException(message: String) extends Exception(message)
