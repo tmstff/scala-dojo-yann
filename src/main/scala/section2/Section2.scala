c89f46aeb39ddce9a4b31149a7c0952b617db9b1
Tim Steffens <tim.steffens@rewe-digital.com>
2016-07-27 08:42:13 +0200

    add solutions for section2

--- src/main/scala/section2/Section2.scala
+++ src/main/scala/section2/Section2.scala
@@ -4,24 +4,31 @@ object Section2 {
 
   // Arrays
 
-  def loud(input: Array[String]): Array[String] = ???
+  def loud(input: Array[String]): Array[String] = input.map( _.toUpperCase )
 
-  def concatenate(input: Array[String]): String = ???
+  def concatenate(input: Array[String]): String = input.mkString( " " )
 
   // Seq and List
 
-  def loud(input: Seq[String]): Seq[String] = ???
+  def loud(input: Seq[String]): Seq[String] = input.map( _.toUpperCase )
 
-  def extractWords(input: Seq[String]): Seq[String] = ???
+  def extractWords(input: Seq[String]): Seq[String] = input.flatMap( _.split(" ") )
 
   // Map
 
-  def loud(input: Map[String, String]): Map[String, String] = ???
+  def loud(input: Map[String, String]): Map[String, String] =
+    input.map { case (k, v) => (k.toUpperCase(), v.toUpperCase()) }
 
-  def counts(input: Seq[String]): Map[String, Int] = ???
+  def counts(input: Seq[String]): Map[String, Int] =
+    extractWords( input ).groupBy( identity ).map{ case (k,v) => (k, v.size) }
 
-  def addTriedCharacter(wordId: Int, character: Char): Unit = ???
+  import scala.collection.mutable.Set
+  val triedChars = Set[Tuple2[Int, Char]]()
 
-  def isCharacterAlreadyTried(wordId: Int, character: Char): Boolean = ???
+  def addTriedCharacter(wordId: Int, character: Char): Unit =
+    triedChars.add( (wordId, character) )
+
+  def isCharacterAlreadyTried(wordId: Int, character: Char): Boolean =
+    triedChars.contains( (wordId, character) )
 
 }
